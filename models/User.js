a6dacd550f52a7faf7b30c6dfed19ff99095785d
devUser110 <jr.devon@gmail.com>
2023-12-11 18:45:48 -0500

    answers for lesson 10

--- models/User.js
+++ models/User.js
@@ -21,7 +21,7 @@ const UserSchema = new mongoose.Schema({
   },
   password: {
     type: String,
-    required: [true, "Please provide a password]"],
+    required: [true, "Please provide a password"],
     minLength: 6,
   },
 });
